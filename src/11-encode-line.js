836dc292d22f5edcc8a0e061491f7c432996d675
Diana Yankovskaya <dianashpandaruk@mail.ru>
2021-04-09 00:00:47 +0300

    feat: implement encodeLine function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = [str[0]];
+  let counter = 1;
+  for (let i = 1; i < str.length; i++) {
+    if (arr[arr.length - 1] !== str[i]) {
+      arr.push(str[i]);
+      counter = 1;
+    } else if (counter === 1) {
+      ++counter;
+      arr.splice(arr.length - 1, 0, counter);
+    } else {
+      ++counter;
+      arr.splice(arr.length - 2, 1, counter);
+    }
+  }
+  return arr.join('');
 }
 
 module.exports = encodeLine;
