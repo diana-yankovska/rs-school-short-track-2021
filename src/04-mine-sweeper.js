b22e762a36313d4e9f17044d3cd5f13b96576e26
Diana Yankovskaya <dianashpandaruk@mail.ru>
2021-04-11 12:15:24 +0300

    feat: implement minesweeper function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,45 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+
+function minesweeper(matrix) {
+  const resultMatrix = [];
+  for (let i = 0; i < matrix.length; i++) {
+    const resultMatrixSubarr = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j]) {
+        resultMatrixSubarr.push(1);
+      } else {
+        resultMatrixSubarr.push(0);
+        if (matrix[i - 1] !== undefined && matrix[i - 1][j - 1]) {
+          ++resultMatrixSubarr[j];
+        }
+        if (matrix[i - 1] !== undefined && matrix[i - 1][j]) {
+          ++resultMatrixSubarr[j];
+        }
+        if (matrix[i - 1] !== undefined && matrix[i - 1][j + 1]) {
+          ++resultMatrixSubarr[j];
+        }
+        if (matrix[i][j - 1] !== undefined && matrix[i][j - 1]) {
+          ++resultMatrixSubarr[j];
+        }
+        if (matrix[i][j + 1] !== undefined && matrix[i][j + 1]) {
+          ++resultMatrixSubarr[j];
+        }
+        if (matrix[i + 1] !== undefined && matrix[i + 1][j - 1]) {
+          ++resultMatrixSubarr[j];
+        }
+        if (matrix[i + 1] !== undefined && matrix[i + 1][j]) {
+          ++resultMatrixSubarr[j];
+        }
+        if (matrix[i + 1] !== undefined && matrix[i + 1][j + 1]) {
+          ++resultMatrixSubarr[j];
+        }
+      }
+    }
+    resultMatrix.push(resultMatrixSubarr);
+  }
+  return resultMatrix;
 }
 
 module.exports = minesweeper;
