db6f1b3e34969bc7be133c89e32f12677ea00a9a
Diana Yankovskaya <dianashpandaruk@mail.ru>
2021-04-11 17:19:20 +0300

    feat: implement findIndex function

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,26 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let startIndex = 0;
+  let endIndex = array.length - 1;
+
+  while (startIndex <= endIndex) {
+    const halfIndex = Math.trunc((startIndex + endIndex) / 2);
+    if (value === array[halfIndex]) {
+      // value is found
+      return halfIndex;
+    }
+    if (value < array[halfIndex]) {
+      // search in left half moving endIndex
+      endIndex = halfIndex - 1;
+    } else {
+      // search in right half moving startIndex
+      startIndex = halfIndex + 1;
+    }
+  }
+  // value not found in array
+  return -1;
 }
 
 module.exports = findIndex;
